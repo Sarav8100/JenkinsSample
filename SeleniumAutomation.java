57b80bbf73a16a53cd7e41a7568744d4835bd2c9
Sarav8100 <68325861+Sarav8100@users.noreply.github.com>
2021-06-06 09:26:20 +0530

    Update SeleniumAutomation.java

--- SeleniumAutomation.java
+++ SeleniumAutomation.java
@@ -3,5 +3,6 @@ public class SeleniumAutomation
 public static void main (String args[])
 {
 	System.out.println("Right now doing Selenium project");
+	System.out.println("Jenkins sample project");
 }
 }
